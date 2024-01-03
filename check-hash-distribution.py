a7ebcb3eac0210fe6416613f30a23cfcc2f284d6
Olivier Léobal <olivier.leobal@owkin.com>
2024-01-03 16:31:04 +0100

    add script for checking hash properties

---
+++ check-hash-distribution.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import hashlib
+import matplotlib.pyplot as plt
+
+counts=[0 for _ in range(256)]
+
+# this file is standard on some unix distributions but might need installing some package
+with open("/usr/share/dict/words") as file:
+    while line := file.readline():
+        i = int.from_bytes(hashlib.shake_256(line.rstrip().encode('utf-8')).digest(1))
+        counts[i] += 1
+        if i == 0 or i == 255:
+            print(f"{i:>3}", line.rstrip())
+
+plt.plot([i for i in range(256)], counts)
+plt.show()
+
